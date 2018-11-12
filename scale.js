c633dc7da50d1b791dfb9d1715669b8c0f26a15c
郭维嘉 <guoweijia@guoweijiadeMacBook-Pro.local>
2018-11-12 20:22:48 +0800

    11

--- scale.js
+++ scale.js
@@ -1,9 +1,14 @@
-//参数为  子元素id,父元素id
 function Drag(son, father) {
+  var position = {
+    id: son,
+    width: '',
+    height: '',
+    x: '',
+    y: ''
+  }
   var son = document.getElementById(son)
   var box = document.getElementById(father)
   son.onmousedown = function(e) {
-    // 先获取拖拽前鼠标相对于当前元素的位置
     var X = son.offsetLeft + box.offsetLeft
     var x2 = e.clientX - X
     var Y = son.offsetTop + box.offsetTop
@@ -13,7 +18,7 @@ function Drag(son, father) {
     document.onmousemove = function(e) {
       son.style.left = e.clientX - box.offsetLeft - x2 + 'px'
       son.style.top = e.clientY - box.offsetTop - y2 + 'px'
-      // 阻止拖动到盒子外
+
       if (son.offsetLeft > box.clientWidth - son.clientWidth)
         son.style.left = box.clientWidth - son.clientWidth + 'px'
       if (son.offsetLeft < 0) son.style.left = 0 + 'px'
@@ -28,11 +33,29 @@ function Drag(son, father) {
     document.onmousemove = null
     son.style.cursor = 'unset'
   }
+  // 鼠标抬起获取位置
+  son.onmouseup = function() {
+    position.width = son.offsetWidth
+    position.height = son.offsetHeight
+    position.x = son.offsetLeft
+    position.y = son.offsetTop
+    //实际数据
+    // position.newWidth = son.offsetWidth / (360 / window.imgWidth) + 'px'
+    // position.newHeight = (son.offsetHeight / son.offsetWidth) * parseFloat(position.newWidth) + 'px'
+    // position.newX = son.offsetLeft / (360 / window.imgWidth) + 'px'
+    // position.newY = son.offsetTop / (360 / window.imgWidth) + 'px'
+    console.log(position)
+  }
 }
 
-// 放大缩小
-// 参数为被放大元素id,父元素id,右下角拖拽点id,缩放最小宽度(可省略,默认100px)
-function Scale(son, father, pot, min) {
+function Scale(son, father, pot, min, freedom) {
+  var position = {
+    id: son,
+    width: '',
+    height: '',
+    x: '',
+    y: ''
+  }
   var pot = document.getElementById(pot)
   var son = document.getElementById(son)
   var father = document.getElementById(father)
@@ -40,40 +63,52 @@ function Scale(son, father, pot, min) {
     e.preventDefault()
     event.stopPropagation()
     father.style.cursor = 'se-resize'
-    // 先获取拖拽前鼠标的坐标
+
     var X = e.clientX
     var Y = e.clientY
     var width = son.clientWidth
     var height = son.clientHeight
-    var rat = width / height //获取宽高比例，缩放时等比例缩放
+    var rat = width / height
     min = min || 100
     document.onmousemove = function(e) {
       e.preventDefault()
       e.stopPropagation()
       son.style.width = width + e.clientX - X + 'px'
-      son.style.height = height + (e.clientX - X) / rat + 'px' // 缩放保持正方形
-      // son.style.height = height + e.clientY - Y + 'px'  // 随意缩放
-      //阻止缩小小于最小大小 (100是写死的，根据图片大小决定)
+      console.log(freedom)
+      if (freedom) son.style.height = height + e.clientY - Y + 'px'
+      else son.style.height = height + (e.clientX - X) / rat + 'px'
+
       if (son.clientWidth < min) {
         son.style.width = min + 'px'
         son.style.height = min / rat + 'px'
       }
-      // if (son.clientHeight < height) son.style.height = height + 'px'
-      // 阻止放大超过盒子
-      //宽度到达边缘  高度也停止增加
       if (son.clientWidth > father.clientWidth - son.offsetLeft) {
         son.style.width = father.clientWidth - son.offsetLeft + 'px'
-        son.style.height = (father.clientWidth - son.offsetLeft) / rat + 'px'
+        if (freedom) son.style.height = son.clientHeight + 'px'
+        else son.style.height = (father.clientWidth - son.offsetLeft) / rat + 'px' // 等比例
       }
-      // 高度到达边缘  宽度也停止增加
       if (son.clientHeight > father.clientHeight - son.offsetTop) {
         son.style.height = father.clientHeight - son.offsetTop + 'px'
-        son.style.width = (father.clientHeight - son.offsetTop) * rat + 'px'
+        if (freedom) son.style.width = son.clientWidth + 'px'
+        else son.style.width = (father.clientHeight - son.offsetTop) * rat + 'px' // 等比例
       }
     }
   }
   father.onmouseup = function() {
     pot.onmousemove = null
     father.style.cursor = 'unset'
+  }
+  // 鼠标抬起获取位置
+  son.onmouseup = function() {
+    position.width = son.offsetWidth
+    position.height = son.offsetHeight
+    position.x = son.offsetLeft
+    position.y = son.offsetTop
+    //实际数据
+    // position.newWidth = son.offsetWidth / (360 / window.imgWidth) + 'px'
+    // position.newHeight = (son.offsetHeight / son.offsetWidth) * parseFloat(position.newWidth) + 'px'
+    // position.newX = son.offsetLeft / (360 / window.imgWidth) + 'px'
+    // position.newY = son.offsetTop / (360 / window.imgWidth) + 'px'
+    console.log(position);	
   }
 }
